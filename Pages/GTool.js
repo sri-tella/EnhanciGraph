95a445c33a1e53296177cbb95edfd48dfd84f29a
tsmanjusha5 <tellasrimanjusha@gmail.com>
2023-12-07 01:08:35 -0600

    Initial commit

---
+++ Pages/GTool.js
@@ -0,0 +1,33 @@
+
+console.log("Script loaded successfully!");
+
+function showHomeContent() {
+    var homeTab = document.getElementById('home-tab');
+    var bengiTab = document.getElementById('bengi-tab');
+    var abcTab = document.getElementById('abc-tab');
+
+    homeTab.style.display = 'block';
+    bengiTab.style.display = 'none'; // Show the content
+    abcTab.style.display = 'none';  // Hide the other content
+
+}
+
+function showBengiContent() {
+    var bengiTab = document.getElementById('bengi-tab');
+    var abcTab = document.getElementById('abc-tab');
+    var homeTab = document.getElementById('home-tab');
+
+    bengiTab.style.display = 'block'; // Show the content
+    abcTab.style.display = 'none';  // Hide the other content
+    homeTab.style.display = 'none';
+}
+
+function showABCContent() {
+    var bengiTab = document.getElementById('bengi-tab');
+    var abcTab = document.getElementById('abc-tab');
+    var homeTab = document.getElementById('home-tab');
+
+    abcTab.style.display = 'block';
+    bengiTab.style.display = 'none';
+    homeTab.style.display = 'none';
+}
