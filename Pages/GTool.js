47c521b3b2fda2153bf5eb7427e58b1948ec4e6a
tsmanjusha5 <tellasrimanjusha@gmail.com>
2024-03-01 04:35:31 -0600

    updated

--- Pages/GTool.js
+++ Pages/GTool.js
@@ -5,10 +5,12 @@ function showHomeContent() {
     var homeTab = document.getElementById('home-tab');
     var bengiTab = document.getElementById('bengi-tab');
     var abcTab = document.getElementById('abc-tab');
+    var abcmaxTab = document.getElementById('abc-max-tab');
 
     homeTab.style.display = 'block';
-    bengiTab.style.display = 'none'; // Show the content
-    abcTab.style.display = 'none';  // Hide the other content
+    bengiTab.style.display = 'none';
+    abcTab.style.display = 'none';
+    abcmaxTab.style.display = 'none';
 
 }
 
@@ -16,19 +18,36 @@ function showBengiContent() {
     var bengiTab = document.getElementById('bengi-tab');
     var abcTab = document.getElementById('abc-tab');
     var homeTab = document.getElementById('home-tab');
+    var abcmaxTab = document.getElementById('abc-max-tab');
 
-    bengiTab.style.display = 'block'; // Show the content
-    abcTab.style.display = 'none';  // Hide the other content
+    bengiTab.style.display = 'block';
+    abcTab.style.display = 'none';
     homeTab.style.display = 'none';
+    abcmaxTab.style.display = 'none';
 }
 
 function showABCContent() {
     var bengiTab = document.getElementById('bengi-tab');
     var abcTab = document.getElementById('abc-tab');
     var homeTab = document.getElementById('home-tab');
+    var abcmaxTab = document.getElementById('abc-max-tab');
 
     abcTab.style.display = 'block';
     bengiTab.style.display = 'none';
     homeTab.style.display = 'none';
+    abcmaxTab.style.display = 'none';
+}
+
+function showABCMaxContent() {
+    var bengiTab = document.getElementById('bengi-tab');
+    var abcTab = document.getElementById('abc-tab');
+    var homeTab = document.getElementById('home-tab');
+    var abcmaxTab = document.getElementById('abc-max-tab');
+
+    abcmaxTab.style.display = 'block';
+    abcTab.style.display = 'none';
+    bengiTab.style.display = 'none';
+    homeTab.style.display = 'none';
+
 }
 
